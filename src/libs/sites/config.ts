70d9d690a6f206340f49cbe147be4f0e2626c0cd
Haytsir <alswo7310@naver.com>
2024-11-25 22:34:05 +0900

    Edit: Changed App Config name style from camelCase to snake_case

--- src/libs/sites/config.ts
+++ src/libs/sites/config.ts
@@ -1,11 +1,11 @@
 import { ConfigModal } from "../../components/config-modal";
 
 export type ConfigData = {
-    autoAppUpdate: boolean;
-    autoLibUpdate: boolean;
-    captureInterval: number;
-    captureDelayOnError: number;
-    useBitBltCaptureMode: boolean;
+    auto_app_update: boolean;
+    auto_lib_update: boolean;
+    capture_interval: number;
+    capture_delay_on_error: number;
+    use_bit_blt_capture_mode: boolean;
     changed?: boolean;
 }
 
